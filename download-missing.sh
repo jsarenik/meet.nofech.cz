a3c0fe9bebc5e8992fd19d060ffd5e6828ecec67
Jan Sarenik <jajomojo@gmail.com>
2020-04-07 12:51:49 +0200

    Initial commit

---
+++ download-missing.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# usage: while true; do ./download-missing.sh ; done
+
+URL=https://meet.nofech.cz
+
+myget() {
+  OUT="public/$1"
+  echo "$OUT" | grep -q '/$' && OUT=${OUT}index.html
+  mkdir -p ${OUT%/*}
+  wget -c -O "$OUT" "$URL/$1"
+#  ./fixbaseurl.sh
+}
+
+timeout 10 ./run-site.sh 2>&1 | grep -B1 "response:404" | grep url: \
+  | cut -d/ -f2- \
+  | while read a; do myget $a; done
