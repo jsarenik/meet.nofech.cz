b110a9d9a1ae6a892f82bcab8ce16ff09ace780e
Jan Sarenik <jajomojo@gmail.com>
2020-04-20 13:51:31 +0200

    update-mee.sh: Include make-mee in update-mee

--- make-mee.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sed -i '/defaultLanguage:/s|cs|en|' public/config.js
-./statify.sh
--- update-mee.sh
+++ update-mee.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-./make-mee.sh
+sed -i '/defaultLanguage:/s|cs|en|' public/config.js
+./statify.sh
 rsync -av --delete . kenti:web/mee/
 git reset --hard
