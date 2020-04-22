41e9cdf55da6952086b198e92e9e3816be0ffff4
Jan Sarenik <jajomojo@gmail.com>
2020-04-22 11:21:16 +0200

    update-lib.sh: Keep in sync with 0e573bc of my meet.jit.si repo

--- update-lib.sh
+++ update-lib.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
 JITSRC=$HOME/src/jitsi-meet
+JMCOPY=$HOME/web/meet.jit.si/public
+a="/$0"; a=${a%/*}; a=${a:-.}; a=${a#/}/; BINDIR=$(cd $a; pwd)
 
-for i in libs css sounds connection_optimization
+mycp() {
+  for i in "$@"
+  do
+    IFDIR=public/${i%/*}
+    test "$IFDIR" = "" -a -d $IFDIR || mkdir -p $BINDIR/$IFDIR
+    cp -v "$i" "$BINDIR/public/$i"
+  done
+}
+
+for i in images libs css sounds connection_optimization
 do
   rsync -av --delete $JITSRC/$i/ public/$i/
 done
-rsync -av $JITSRC/images/ public/images/
 rsync -av $JITSRC/lang/ public/lang/
 rsync -av $JITSRC/static/ $JITSRC/index.html static/
 
-cp -v \
-  $JITSRC/plugin.head.html \
-  $JITSRC/head.html \
-  $JITSRC/base.html \
-  $JITSRC/title.html \
-  $JITSRC/body.html \
-  public/
+cd $JMCOPY
+cp -v static/welcomePageAdditionalContent.html $BINDIR/static/
+mycp css/all.css images/add_to_slack.png images/welcome_page/appstore.svg images/welcome_page/facebook.svg images/welcome_page/github.svg images/welcome_page/googleplay.png images/welcome_page/linkedin.svg images/welcome_page/twitter.svg
+
+cd $JITSRC
+mycp plugin.head.html head.html base.html title.html body.html
+
+cd $BINDIR
 cp -v main-cs.json languages-cs.json public/lang/
 cp -v deep-linking-image.png public/images/
