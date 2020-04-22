ec285e811a379d40ef92a138decc67f63f8ae42d
Jan Sarenik <jajomojo@gmail.com>
2020-04-22 19:01:01 +0200

    Hide watermark

--- public/interface_config.js
+++ public/interface_config.js
@@ -20,7 +20,7 @@ var interfaceConfig = {
     JITSI_WATERMARK_LINK: 'https://jitsi.org',
 
     // if watermark is disabled by default, it can be shown only for guests
-    SHOW_WATERMARK_FOR_GUESTS: true,
+    SHOW_WATERMARK_FOR_GUESTS: false,
     SHOW_BRAND_WATERMARK: false,
     BRAND_WATERMARK_LINK: '',
     SHOW_POWERED_BY: false,
