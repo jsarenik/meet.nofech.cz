84bfa01617535c7e7374fc3a9fa5c93aedbae22c
Jan Sarenik <jajomojo@gmail.com>
2020-05-19 17:19:31 +0200

    Add pngtowebp.sh

---
+++ pngtowebp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+grep -q watermark.png public/css/all.css && \
+  sed -i 's|images/watermark.png|images/watermark.webp|' public/css/all.css
+grep -q googleplay.png static/welcomePageAdditionalContent.html && \
+  sed -i 's|googleplay.png|googleplay.webp|' public/css/all.css
+
+#find public -name '*.png'
+
+cd public/images/welcome_page
+cwebp googleplay.png -o googleplay.webp
+cd ..
+cwebp watermark.png -o watermark.webp
