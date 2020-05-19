dcb493c2e737b42d544800df809e121944da1916
Jan Sarenik <jajomojo@gmail.com>
2020-05-19 17:27:20 +0200

    pngtowebp.sh: Fix welcomePage... file processing

--- pngtowebp.sh
+++ pngtowebp.sh
@@ -3,7 +3,7 @@
 grep -q watermark.png public/css/all.css && \
   sed -i 's|images/watermark.png|images/watermark.webp|' public/css/all.css
 grep -q googleplay.png static/welcomePageAdditionalContent.html && \
-  sed -i 's|googleplay.png|googleplay.webp|' public/css/all.css
+  sed -i 's|googleplay.png|googleplay.webp|' static/welcomePageAdditionalContent.html
 
 #find public -name '*.png'
 
