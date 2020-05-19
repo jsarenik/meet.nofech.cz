4b6f31c0c3e107c276cf5dcd4cac4d53a4f499d4
Jan Sarenik <jajomojo@gmail.com>
2020-05-19 16:20:37 +0200

    statify.sh: do not really statify, load extra files from files

--- statify.sh
+++ statify.sh
@@ -3,20 +3,25 @@
 PROCESS=${*:-"static/*.html"}
 for i in $PROCESS
 do
+#<style type=\"text/css\">\n#include \"public/\1\"\n</style>|" \
+#  -e "/\s*<script><!--#include/\
+#s|^\s*<script><!--#include virtual=\"\([^\"]\+\).*|\
+#<script>\n#include \"public/\1\"\n</script>|" \
+#  -e "/<link rel=\"stylesheet\"/\
+#s|^\s*<link rel=\"stylesheet\" href=\"\([^\"]\+\)\".*$|\
 sed \
   -e "/\s*<!--#include/\
 s|^\s*<!--#include virtual=\"\([^\"]\+\).*|#include \"public/\1\"|" \
   -e "/\s*<script><!--#include/\
 s|^\s*<script><!--#include virtual=\"\([^\"]\+\).*|\
-<script>\n#include \"public/\1\"\n</script>|" \
-  -e "/<link rel=\"stylesheet\"/\
-s|^\s*<link rel=\"stylesheet\" href=\"\([^\"]\+\)\".*$|\
-<style type=\"text/css\">\n#include \"public/\1\"\n</style>|" \
+<script src=\"/\1\"></script>|" \
   $i \
   | cpp \
   | sed -e '/^#/d;/^$/d;s/^\s\+//' \
   -e "/<script src=\"[a-z]/\
 s|^\s*<script src=\"\([a-z]\)|<script src=\"/\1|" \
+  -e "/<script src=\"\/\//\
+s|^\s*<script src=\"\/\/|<script src=\"/|" \
   > public/$i
 done
 cat public/static/index.html > public/index.html
